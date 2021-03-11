25d85528ccfe56f25d1438338e7eb88b4d27e721
cfee89 <craig@Craigs-Air.fios-router.home>
2021-03-11 08:56:24 -0500

    Cleaned up workspace

--- LongestCommonStringApp/src/test/java/com/lcs/algoService/LcsAlgorithmTest.java
+++ LongestCommonStringApp/src/test/java/com/lcs/algoService/LcsAlgorithmTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import org.junit.Test;
 
-public class LcsAlgorithmTests {
+public class LcsAlgorithmTest {
 
 	@Test
 	public void GivenValidSetWithOneCommonString_WhenLongestCommonStringFound_ReturnAsSet() {
